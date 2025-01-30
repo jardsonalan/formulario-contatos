a3f96dbbf8d361e146bd985066d9f8c236254483
jardsonalan <jardsonalan132@gmail.com>
2025-01-30 08:14:39 -0300

    feat: subindo projeto de cadastro de informações em uma tabela

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+import Form from './components/Form.vue';
+import { ref } from 'vue';
+
+type Dados = {
+  nome: string,
+  email: string
+}
+
+const listaDados = ref<Dados[]>([])
+
+const adicionarPessoas = (nome: string, email: string) => {
+  listaDados.value.push({
+    nome: nome,
+    email: email
+  })
+}
+</script>
+
+<template>
+  <Form
+    @cadastrar="adicionarPessoas"
+  />
+  <table>
+    <thead>
+      <tr>
+        <th>Nome</th>
+        <th>E-mail</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr
+        v-for="(item, index) in listaDados"
+        :key="index"
+      >
+        <td>
+          {{ item.nome }}
+        </td>
+        <td>
+          {{ item.email }}
+        </td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/Form.vue
@@ -0,0 +1,55 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const pararEnvio = (evt:Event) => {
+  evt.preventDefault()
+}
+
+const nome = ref('')
+const email = ref('')
+
+console.log(nome, email)
+</script>
+
+<template>
+  <form class="form-cadastro" @submit="pararEnvio">
+    <div class="nome-input">
+      <label for="nome">Nome</label>
+      <input type="text" id="nome" v-model="nome">
+    </div>
+    <div class="email-input">
+      <label for="email" id="email">E-mail</label>
+      <input type="text" id="email" v-model="email">
+    </div>
+    <button @click="$emit('cadastrar', nome, email)">Cadastrar</button>
+  </form>
+</template>
+
+<style scoped>
+  .form-cadastro {
+    background-color: rgb(132, 181, 255);
+    padding: 10px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  .form-cadastro > .nome-input, .form-cadastro > .email-input {
+    display: flex;
+    flex-direction: column;
+    width: 80%;
+    margin: 0 auto;
+  }
+
+  .form-cadastro > .nome-input > input, .form-cadastro > .email-input > input {
+    padding: 4px;
+  }
+
+  .form-cadastro > button {
+    margin: 10px 0 0 0;
+    padding: 6px;
+    width: 80%;
+    cursor: pointer;
+  }
+</style>
